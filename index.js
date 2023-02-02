c442377e745014661805fcd9de4d594866ed4850
Oliver <olichase91@gmail.com>
2023-02-02 12:08:06 -0800

    Solved Lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solutions in this file
+
+// for (let age = 30; age < 40; age++){
+//     console.log(`I'm ${age} years old. Happy birthday to me!`)
+// }
+
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts){
+//     for (let i = 0; i < gifts.length; i ++){
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`)
+//     }
+//     return gifts
+// }
+
+// wrapGifts(gifts)
+
+const cards = []
+
+function writeCards(array, event){
+    for(let x = 0; x < array.length; x++){
+        cards[x] = `Thank you, ${array[x]}, for the wonderful ${event} gift!`
+    }
+    return cards
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+console.log(cards)
+
+function countDown (y){
+    while (y + 1 > 0){
+        y--
+        console.log(y + 1)
+    }
+}
+
+countDown(10)
